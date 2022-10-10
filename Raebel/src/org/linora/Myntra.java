48e5156bada40ddd190defa5418c9151c4515f58
vivek22911 <antony.vivek980@gmail.com>
2022-10-10 19:13:18 +0530

    Second Commit

---
+++ Raebel/src/org/linora/Myntra.java
@@ -0,0 +1,28 @@
+package org.linora;
+
+import java.awt.AWTException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Myntra {
+	public static void main(String[] args) throws InterruptedException, AWTException {
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\VIVEK\\eclipse-workspace\\SeleniumBrowser\\driver\\geckodriver.exe");
+		
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.get("https://www.myntra.com/register?");
+		
+		driver.manage().window().maximize();					
+		
+		WebElement p = driver.findElement(By.xpath("//input[@autocomplete='new-password']"));
+		p.sendKeys("9442251206");
+		
+		WebElement q = driver.findElement(By.xpath("//div[@class='submitBottomOption']"));
+		q.click();
+			
+	}
+}
